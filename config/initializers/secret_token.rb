ff629975c7df6cd1a98b7d08c678e792e7f683eb
andykunity <andyk@unity3d.com>
2014-04-10 09:56:23 -0700

    Initalize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '67b64edec84ddab57a3b8f695b781624235795713c2c37a571fb5deecdd87357fe2cdb4b6687a81800a5e9eb1883d5f44a8254e43926c425e543ee4985b52275'
